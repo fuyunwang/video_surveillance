5ad80b85c6453e72344e0e445629a8e8819cf47d
fuyunwang <2375872953@qq.com>
2020-10-13 12:47:58 +0800

    algorithm data insert to mysql

--- chuoyue-algorithm-server/main.py
+++ chuoyue-algorithm-server/main.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import os
 import shutil
 import numpy as np
@@ -10,8 +11,9 @@ import qiniu.config
 import json
 import html
 import pandas as pd
-from sqlalchemy import SQLAlchemy
+import pymysql
 from flask_cors import CORS
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app, resources=r'/*')
@@ -19,6 +21,18 @@ CORS(app, resources=r'/*')
 # qiniu 相关
 q = Auth('KvYtKCG1D3bpwb8O36HcZR0DdVOFU5q9qIyB1Tp4', 'rOXLGzS6QaCLIFYkkDuWpv49_x5m_wb-sFgC_9Zo')
 bucket_name = 'chuoyue-video-surveillance'
+
+# mysql 相关
+base_url = 'http://qi2c9qbdt.hb-bkt.clouddn.com/'
+conn= pymysql.connect(
+        host='localhost',
+        port= 3306,
+        user='root',
+        password='password',
+        db='video_surveillance',
+        charset= 'utf8'
+        )
+cur = conn.cursor()
 
 
 @app.route('/')
@@ -33,6 +47,8 @@ def video_show():
             'status': 40000,
             'message': 'please request with post'
         }
+        cur.close()
+        conn.close()
         response = make_response(jsonify(result))
         response.status = "400"
         response.headers['Access-Control-Allow-Origin'] = '*'
@@ -43,8 +59,10 @@ def video_show():
     json_data = json.loads(params.decode('utf-8'))
     yolo = YOLO()
     # 调用摄像头
-    print(json_data.get('video_url'))
+    video_url = json_data.get('video_url')
+    print(video_url)
     capture = cv2.VideoCapture(json_data.get('video_url'))  # capture=cv2.VideoCapture("1.mp4")
+    cur.execute("update department set status=1 where screenShot = {}".format("'"+video_url+"'"))
     i = 0
     beforeNum = 0
     try:
@@ -70,6 +88,12 @@ def video_show():
                 token = q.upload_token(bucket_name, key)
                 localfile = './static/video/'+'person_detect'+str(i)+'.jpg'
                 ret, info = put_file(token, key, localfile)
+                data_url = base_url+'person_detect'+str(i)+'.jpg'
+                cur.execute("select * from department where screenShot = {}".format("'"+video_url+"'"))
+                all = cur.fetchall()
+                cur.execute("insert into department_solved(departmentName,alarmTime,incidentType,deviceName,screenShot,contact,note) values("+"'"+all[0][1]+"'"
+                            +","+"'"+datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')+"'"+","+"'"+all[0][3]+"'"+","+"'"+all[0][4]+"'"+","+"'"+data_url+"'"+","+"'2375872953@qq.com'"+","+"'已处理'"+")")
+                conn.commit()
                 print(info)
             beforeNum = tmp[1]
 
@@ -77,6 +101,8 @@ def video_show():
             'status': 20000,
             'message': '当前视频检测完成'
         }
+        cur.close()
+        conn.close()
         response = make_response(jsonify(result))
         response.status = "200"
         response.headers['Access-Control-Allow-Origin'] = '*'
@@ -84,6 +110,8 @@ def video_show():
         response.headers['Access-Control-Allow-Headers'] = 'x-requested-with'
         return response
     except:
+        cur.close()
+        conn.close()
         result = {
             'status': 50000,
             'message': '视频检测失败'
