310ce25c349538aaec0eed2f3472039751ab4ff8
fuyunwang <2375872953@qq.com>
2020-11-06 21:44:33 +0800

    reversion swagger add token

--- Chuoyue-SpringBoot/chuoyue-web/src/main/java/com/fuyunwang/chuoyue/config/Swagger2Config.java
+++ Chuoyue-SpringBoot/chuoyue-web/src/main/java/com/fuyunwang/chuoyue/config/Swagger2Config.java
@@ -4,13 +4,18 @@ import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @description:
@@ -23,12 +28,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Swagger2Config {
     @Bean
     public Docket createRestApi() {
+        ParameterBuilder ticketPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<Parameter>();
+        ticketPar.name("Authorization").description("Global token for authentication")//Token 以及Authorization 为自定义的参数，session保存的名字是哪个就可以写成那个
+                .modelRef(new ModelRef("string")).parameterType("header")
+                .required(false).build(); //header中的ticket参数非必填，传空也可以
+        pars.add(ticketPar.build());
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+//                .securitySchemes(securitySchemes())
+//                .securityContexts(securityContexts())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.fuyunwang.chuoyue"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalOperationParameters(pars);
     }
 
     private ApiInfo apiInfo() {
@@ -40,4 +55,33 @@ public class Swagger2Config {
                 .version("2.0")
                 .build();
     }
+
+
+
+    /*public List<ApiKey> securitySchemes(){
+        List<ApiKey> apiKeys=new ArrayList<>();
+        apiKeys.add(new ApiKey("oauth2 认证","Authorization","bearer "));
+
+        return apiKeys;
+    }
+
+    public List<SecurityContext> securityContexts(){
+        List<SecurityContext> securityContexts=new ArrayList<>();
+        securityContexts.add(SecurityContext.builder()
+                .securityReferences(securityReferences())
+                .forPaths(PathSelectors.any()).build());
+
+        return securityContexts;
+    }
+
+    public List<SecurityReference> securityReferences(){
+        AuthorizationScope[] authorizationScopes=new AuthorizationScope[1];
+        authorizationScopes[0]=new AuthorizationScope("global","accessEverything");
+
+        List<SecurityReference> securityReferences=new ArrayList<>();
+        securityReferences.add(new SecurityReference("Authorization",authorizationScopes));
+
+        return securityReferences;
+    }*/
+
 }
